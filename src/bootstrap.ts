063cd587d82096a894e6d842e3c176927f79d800
Mashhood Rastgar <mashhoodr@gmail.com>
2016-05-05 15:49:48 +0500

    Added link to list route

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,12 +1,14 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteConfig} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/Rx'; //load all features
+
 import {Header} from './shell/header.component';
 import {CountriesList} from './countries-list/countries-list.component';
 import {CountryInfo} from './country-info/country-info.component';
-import {HTTP_PROVIDERS} from 'angular2/http';
+
 import {geonames} from './geonames.service';
-import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteConfig} from 'angular2/router';
-import 'rxjs/Rx'; //load all features
 
 @RouteConfig([
   {
