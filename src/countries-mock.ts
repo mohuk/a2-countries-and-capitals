b6a55e96030c7e69b1b99f1351741ed0491905c8
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2016-05-05 15:21:31 +0500

    abiding by the interface

--- src/countries-mock.ts
+++ src/countries-mock.ts
@@ -2,40 +2,40 @@ import {ICountry} from './country';
 
 export var COUNTRIES : ICountry[] = [
   {
-    name: 'Pakistan',
-    countryCode: 1,
+    countryName: 'Pakistan',
+    countryCode: 'PK',
     capital: 'Islamabad',
     areaInSqKm: 1280000,
     population: 5500000,
     continent: 'Asia'
   },
   {
-    name: 'India',
-    countryCode: 2,
+    countryName: 'India',
+    countryCode: 'IN',
     capital: 'New Delhi',
     areaInSqKm: 12990000,
     population: 889500000,
     continent: 'Asia'
   },
   {
-    name: 'China',
-    countryCode: 3,
+    countryName: 'China',
+    countryCode: 'CN',
     capital: 'Beijing',
     areaInSqKm: 9999999,
     population: 9999999,
     continent: 'Asia'
   },
   {
-    name: 'Afghanistan',
-    countryCode: 4,
+    countryName: 'Afghanistan',
+    countryCode: 'AF',
     capital: 'Kabul',
     areaInSqKm: 120000,
     population: 400000,
     continent: 'Asia'
   },
   {
-    name: 'Saudia Arabia',
-    countryCode: 5,
+    countryName: 'Saudia Arabia',
+    countryCode: 'KSA',
     capital: 'Riyadh',
     areaInSqKm: 189000,
     population: 65000,
